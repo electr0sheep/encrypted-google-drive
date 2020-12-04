19ab85276c4ace6c0b44d947620126e4922fd986
electr0sheep <electr0sheep@electr0sheep.com>
2020-12-04 10:29:03 -0700

    move api key to env variable

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import vuetify from './plugins/vuetify';
 import VueGapi from 'vue-gapi';
 
 const config = {
-  apiKey: 'AIzaSyDVGL6dh5KIGfECVxWfR4BEvlAZ8fn-OXA',
+  apiKey: process.env.API_KEY,
   clientId: '102304335748-hql78fc75ij3fhv2eo1ee35hm7ckcmub.apps.googleusercontent.com',
   scope: 'https://www.googleapis.com/auth/drive',
   discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
